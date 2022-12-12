4994073b73e571d1d9c404c4bf19aee3e8e0e1d2
fatemahAlMomen <fatemah.alm@hotmail.com>
2022-12-12 13:19:16 +0300

    CW6

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# write your code here
+# part 1
+person={
+    'name':'Ali',
+    'age':'20',
+    'hobbies':['horse riding','painting','swimming']
+}
+print(person['name'])
+print(person['age'])
+
+#part 2
+person['age']=21
+person['country']='Kuwait'
+print(person)
+print(len(person))
+
+#part 3
+person['hobbies'].append('reading')
+
+def check_hobby(person):
+    if len(person['hobbies']) >= 3:
+        print('WOW you are amazing!')
+
+check_hobby(person)
